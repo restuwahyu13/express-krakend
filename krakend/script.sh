6e9061b4aef073d1ca0683a0e99592dd3cab2335
restuwahyu13 <restuwahyu13@gmail.com>
2021-04-13 15:48:52 +0700

    update krakend config

--- krakend/script.sh
+++ krakend/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FC_ENABLE=1 \
-  FC_SETTINGS="/etc/krakend/configs/endpoints" \
-  FC_PARTIALS="/etc/krakend/configs/plugins" \
-  krakend run -d -c "/etc/krakend/krakend.json"
+  FC_SETTINGS="/configs/endpoints" \
+  FC_PARTIALS="/configs/plugins" \
+  krakend run -d -c /krakend.json
